7f7f735acb7ffffa436f90ec77f4dd6ae62d43d7
thatresolvestcg <thatresolvestcg@gmail.com>
2019-12-04 22:14:55 +0000

    started to build navbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import navBar from './navBar';
 
 class App extends React.Component {
     render() {
         return (
             <div className='hello'>
                 <h1>hello world</h1>
+                <div className='navBar'>
+
+                </div>
             </div>
         )
     }
---
+++ src/components/navBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/navBar.css';
+
+class navBar extends React.Component {
+    render() {
+        return (
+            <div className='navBar'>
+                <h1>test</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default navBar;
